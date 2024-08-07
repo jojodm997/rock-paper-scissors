2d5323c6b0efe780efd5a64cafafc7bddbea0024
Fauzan <fauzandwimulyono@gmail.com>
2024-08-07 18:07:03 +0700

    Added 5 round

--- script.js
+++ script.js
@@ -55,9 +55,14 @@ function playRound(humanChoice, computerChoice) {
   
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
+function playGame() {
+  for (let i = 0; i < 5; i++) {
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection);
+  }
+}
 
-playRound(humanSelection, computerSelection);
+playGame();
 
 alert(`Human score is ${humanScore} and Computer score is ${computerScore}`);
